01862104b4072cd5260a5660a100ef2fb7877457
John Elliott <johnelliott703@gmail.com>
2016-03-01 10:08:02 -0500

    wip testing

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var ecstatic = require('ecstatic')({ root: __dirname + '/sounds', handleError: f
 var Sonos = require('sonos').Sonos;
 var say = require("./lib/say").speak;
 
-// arp -a |grep b8:e9:37:8b:c7:c8
+var host = process.env.SONOS_HOST || '10.28.63.170';
+var sonosPort = process.env.SONOS_PORT || 1400;
+var localIp = '10.28.63.122';
 // create sonos speaker
-var speakers = new Sonos(process.env.SONOS_HOST || '10.28.63.170', process.env.SONOS_PORT || 1400);
+var speakers = new Sonos(host, sonosPort);
 console.log("speakers created", speakers);
+
+// use ARP to find local sonos by MAC address
+// arp -a |grep b8:e9:37:8b:c7:c8
 
 // sonos.play('https://archive.org/download/testmp3testfile/mpthreetest.mp3', function(err, playing) {
 //   console.log([err, playing]);
@@ -20,7 +25,7 @@ var server = http.createServer(function (request, response) {
 
 server.listen(6818, function(){
 	console.log("server listening");
-	say("Welcome to Social Tables", "sound", function () {
+	say("Hello Social Tables", "sound", function () {
 		console.log("text to speech complete");
 		// TODO ip
 		//speakers.currentTrack(function(err, info) {
@@ -29,7 +34,7 @@ server.listen(6818, function(){
 
 		//TODO this should get your IP automatically.
 		// tell sonos to play this crap
-		speakers.play("http://10.28.63.122:6818/sound.m4a", function(err, playing) {
+		speakers.play("http://10.28.63.122/sound.m4a", function(err, playing) {
 		    if (err) {
 		   	 console.error("There was an error playing:", err);
 		    }
